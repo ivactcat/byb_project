34b4b7696947fd797c54f137066a8aa69d877ca0
ivactcat <ivancatcam@gmail.com>
2023-06-19 16:28:39 +0100

    added file

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+
+name= input("what is your name ?")
+
+print("hello " + name + ".")
+# print("hello_world")
+
+# this is a comment you can write anything here to explain whatever
+print("hello_world")
